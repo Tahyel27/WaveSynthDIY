336f4474b7d133f825ec5d0777f358d5ec4ab087
David Sponer <sponderdavid@gmail.com>
2025-11-18 20:43:41 +0100

    Minimal implementation of AudioDevice class done, still has global state variables and no channels

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,15 +1,35 @@
 #include <stdio.h>
+#include <cmath>
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
 #include "AudioDevice.hpp"
+
+int16_t sinegen(double t, int amp, double f)
+{
+    return amp * sin(f * t * M_PI);
+}
+
+int16_t sample_callback(const AudioDevice::ChannelInfo &info)
+{
+    const int maxamp = 32767 / 2.5;
+    double t = double(info.chunk + info.sample) / double(45045);
+    return sinegen(t,maxamp/5,440);
+}
 
 int main()
 {
     stdio_init_all();
 
+    auto device = AudioDevice(12,13,AudioDevice::DeviceMode::MONO,1);
+
+    device.initialize();
+
     while (true)
     {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        if (GLOBAL_DEVICE_FLAG)
+        {
+            device.update();
+            GLOBAL_DEVICE_FLAG = false;
+        }
     }
 }
