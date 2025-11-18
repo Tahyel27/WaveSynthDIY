4f1795e6f3bfaa82268d865b5c34afc6a0652698
David Sponer <sponderdavid@gmail.com>
2025-11-18 16:26:54 +0100

    Set up firmware project structure

---
+++ firmware/include/AudioDevice.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "audio_device.pio.h"
+
+class AudioDevice
+{
+private:
+    /* data */
+public:
+    AudioDevice(/* args */);
+    ~AudioDevice();
+};
+
+AudioDevice::AudioDevice(/* args */)
+{
+}
+
+AudioDevice::~AudioDevice()
+{
+}
