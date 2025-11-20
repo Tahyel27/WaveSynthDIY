10ffd9c31a0000842b7b458b6dd6617c47e776c5
David Sponer <sponderdavid@gmail.com>
2025-11-20 23:23:05 +0100

    Set up correct output for performing test 1

--- playground/button_registers/button_test_code/button_test_code.cpp
+++ playground/button_registers/button_test_code/button_test_code.cpp
@@ -4,22 +4,30 @@
 
 #include "blink.pio.h"
 
-void blink_program_init(PIO pio, uint sm, uint offset, uint pin)
+void blink_program_init(PIO pio, uint sm, uint offset, uint pinl, uint pinclk)
 {
-    pio_gpio_init(pio, pin);
-    pio_sm_set_consecutive_pindirs(pio, sm, pin, 1, true);
     pio_sm_config c = blink_program_get_default_config(offset);
-    sm_config_set_set_pins(&c, pin, 1);
+    pio_gpio_init(pio, pinl);
+    pio_gpio_init(pio, pinclk);
+    pio_sm_set_consecutive_pindirs(pio, sm, pinl, 1, true);
+    pio_sm_set_consecutive_pindirs(pio, sm, pinclk, 1, true);
+
+    sm_config_set_set_pins(&c, pinl, 1);
+    sm_config_set_sideset_pins(&c, pinclk);
+    sm_config_set_clkdiv_int_frac8(&c, 10, 1);
+
     pio_sm_init(pio, sm, offset, &c);
 }
 
-void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq) {
-    blink_program_init(pio, sm, offset, pin);
+void test1(PIO pio, uint sm, uint offset, uint pinlatch, uint pinclk) {
+    blink_program_init(pio, sm, offset, pinlatch, pinclk);
     pio_sm_set_enabled(pio, sm, true);
 
-    printf("Blinking pin %d at %d Hz\n", pin, freq);
+    //printf("Blinking pin %d at %d Hz\n", pin, freq);
 
-    pio->txf[sm] = (125000000 / (2 * freq)) - 3;
+    //pio->txf[sm] = (125000000 / (2 * freq)) - 3;
+
+    
 }
 
 int main()
@@ -27,11 +35,16 @@ int main()
     stdio_init_all();
 
     // PIO Blinking example
-    PIO pio = pio0;
+    PIO pio;
+    uint sm;
     uint offset = pio_add_program(pio, &blink_program);
+    int pinlatch = 14;
+    int pinclk = 15;
+
+    bool success = pio_claim_free_sm_and_add_program(&blink_program, &pio, &sm, &offset);
     printf("Loaded program at %d\n", offset);
     
-    blink_pin_forever(pio, 0, offset, PICO_DEFAULT_LED_PIN, 3);
+    test1(pio, 0, offset, pinlatch, pinclk);
 
     while (true) {
         printf("Hello, world!\n");
