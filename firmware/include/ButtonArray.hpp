0f23c2c3627a0d3f363c5495305e70b06bcf4d43
David Sponer <sponderdavid@gmail.com>
2025-11-21 19:19:15 +0100

    added ButtonArray files

---
+++ firmware/include/ButtonArray.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <hardware/pio.h>
+#include <pico/stdlib.h>
+
+#include "button_array.pio.h"
+
+class ButtonArray
+{
+private:
+    
+public:
+    ButtonArray(/* args */);
+    ~ButtonArray();
+};
+
+ButtonArray::ButtonArray(/* args */)
+{
+}
+
+ButtonArray::~ButtonArray()
+{
+}
