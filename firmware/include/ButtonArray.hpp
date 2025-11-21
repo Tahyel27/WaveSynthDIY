bf85108165253f8d3f92a04900d3d3409f3de9d6
David Sponer <sponderdavid@gmail.com>
2025-11-21 23:30:26 +0100

    Completed first version of ButtonArray class that can read up to 32 buttons

--- firmware/include/ButtonArray.hpp
+++ firmware/include/ButtonArray.hpp
@@ -3,9 +3,24 @@
 #include <hardware/pio.h>
 #include <hardware/gpio.h>
 #include <array>
+#include <algorithm>
+#include <queue>
+#include <optional>
 #include <pico/stdlib.h>
+#include <stdio.h>
 
 #include "button_array.pio.h"
+
+struct ButtonEvent
+{
+    int button;
+    int deviceId;
+    enum class Type{
+        PRESSED,
+        RELEASED
+    };
+    Type type;
+};
 
 class ButtonArray
 {
@@ -27,13 +42,20 @@ private:
     PIOdata pio;
 
     static const uint MAX_BUTTONS = 256;
-    std::array<char, MAX_BUTTONS> buttons_prev;
+    std::array<int, MAX_BUTTONS> buttons_prev;
 
-    uint deviceID;
+    std::queue<int> pressedQueue;
+    std::queue<int> releasedQueue;
+
+    int deviceID;
 
     void button_array_program_init();
 public:
     void poll();
+
+    bool isPressed(int button);
+
+    std::optional<ButtonEvent> getEvent();
 
     ButtonArray(uint datapin, uint clockpin, uint latchpin);
     ~ButtonArray();
--- firmware/src/ButtonArray.cpp
+++ firmware/src/ButtonArray.cpp
@@ -27,6 +27,78 @@ void ButtonArray::button_array_program_init()
 void ButtonArray::poll()
 {
     pio_sm_clear_fifos(pio.pio, pio.sm);
+    char loopcounter = 0;
+    while (pio_sm_get_rx_fifo_level(pio.pio, pio.sm) < 2)
+    {
+        if (loopcounter > 50)
+        {
+            break;
+        }
+        loopcounter++;
+    }
+    uint32_t word = pio_sm_get_blocking(pio.pio, pio.sm);
+    word = pio_sm_get_blocking(pio.pio, pio.sm);
+
+    int buttons_index = 0;
+    std::array<int, MAX_BUTTONS> buttons;
+    std::fill(buttons.begin(), buttons.end(), -1);
+
+    for (size_t i = 0; i < 32; i++)
+    {
+        const uint32_t mask = 0x7FFFFFFF;
+        bool down = ((word | mask) == 0xFFFFFFFF);
+        word = word << 1;
+        if (down)
+        {
+            if (std::find(buttons_prev.begin(),buttons_prev.end(), i) == buttons_prev.end())
+            {
+                pressedQueue.push(i);
+            }    
+            buttons[buttons_index++] = i;
+        }
+        else
+        {
+            if (std::find(buttons_prev.begin(),buttons_prev.end(), i ) != buttons_prev.end())
+            {
+                releasedQueue.push(i);
+            }
+        }
+    }
+
+    std::copy(buttons.begin(), buttons.end(), buttons_prev.begin());    
+}
+
+bool ButtonArray::isPressed(int button)
+{
+    if (std::find(buttons_prev.begin(),buttons_prev.end(),button) != buttons_prev.end())
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+std::optional<ButtonEvent> ButtonArray::getEvent()
+{
+    if (!pressedQueue.empty())
+    {
+        int button = pressedQueue.front();
+        pressedQueue.pop();
+        return ButtonEvent{button,deviceID, ButtonEvent::Type::PRESSED};
+    }
+    else if (!releasedQueue.empty())
+    {
+        int button = releasedQueue.front();
+        releasedQueue.pop();
+        return ButtonEvent{button, deviceID, ButtonEvent::Type::RELEASED};
+    }
+    else
+    {
+        return std::nullopt;
+    }
+    
 }
 
 ButtonArray::ButtonArray(uint datapin, uint clockpin, uint latchpin)
@@ -38,6 +110,8 @@ ButtonArray::ButtonArray(uint datapin, uint clockpin, uint latchpin)
     pio_claim_free_sm_and_add_program(&button_array_program, &pio.pio, &pio.sm, &pio.offset);
 
     button_array_program_init();
+
+    std::fill(buttons_prev.begin(),buttons_prev.end(),-1);
 }
 
 ButtonArray::~ButtonArray()
