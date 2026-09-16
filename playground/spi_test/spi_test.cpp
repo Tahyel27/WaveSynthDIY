75d8a9734452c90b61c93f2cc75be42c981063c1
David Sponer <sponderdavid@gmail.com>
2026-09-16 10:38:26 +0200

    tested minimal handler at 4 Mhz

--- playground/spi_test/spi_test.cpp
+++ playground/spi_test/spi_test.cpp
@@ -50,35 +50,30 @@ int main()
 {
     stdio_init_all();
 
-    // SPI initialisation. This example will use SPI at 1MHz.
-    spi_init(SPI_PORT, 1000*1000);
-    gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
-    gpio_set_function(PIN_CS,   GPIO_FUNC_SIO);
-    gpio_set_function(PIN_SCK,  GPIO_FUNC_SPI);
-    gpio_set_function(PIN_MOSI, GPIO_FUNC_SPI);
+    // SPI initialisation. This example will use SPI at 4MHz.
     
+    SPIHandler spi(spi0, PIN_MISO, PIN_MOSI, PIN_SCK, 4 * 1000 * 1000);
+
     // Chip select is active-low, so we'll initialise it to a driven-high state
-    gpio_set_dir(PIN_CS, GPIO_OUT);
-    gpio_put(PIN_CS, 1);
     // For more examples of SPI use see https://github.com/raspberrypi/pico-examples/tree/master/spi
 
     sleep_ms(2000);
     printf("starting test\n");
 
     uint8_t byte = 0b0000'0001;
-    spi_write_blocking(SPI_PORT, &byte, 1);
+    spi.write_blocking(&byte, 1);
 
     sleep_ms(1000);
     byte = byte << 1;
-    spi_write_blocking(SPI_PORT, &byte, 1);
+    spi.write_blocking(&byte, 1);
 
     sleep_ms(1000);
     byte = byte << 1;
-    spi_write_blocking(SPI_PORT, &byte, 1);
+    spi.write_blocking(&byte, 1);
 
     sleep_ms(1000);
     byte = byte << 1;
-    spi_write_blocking(SPI_PORT, &byte, 1);
+    spi.write_blocking(&byte, 1);
 
     while (true) {
         printf("Hello, world!\n");
